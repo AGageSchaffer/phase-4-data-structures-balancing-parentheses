1f6be1a7193d9c57e1dbb7eef1e1a22c8d77021c
AustinSchaffer <austinschaffer@gmail.com>
2023-03-02 16:39:48 -0600

    commit tree 46670791f7f1ed186e7bc521b9b953c49c94cb58

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,28 @@
 require_relative './stack'
 
-# your code here
+
+def balancing_parentheses string
+
+    stack = Stack.new()
+    string.split("").each do |c|
+    
+        if stack.peek == "(" && c == ")"
+            stack.pop 
+
+        else
+            stack.push(c)
+        end
+    end
+    
+        #     if c == "("
+    #         stack.push(c)
+    #     elsif stack.peek == "(" && c == ")"
+    #         stack.pop
+    #     elsif c == ")"
+    #         stack.push(c)
+    #     end
+    # end
+
+    return stack.size
+
+end
